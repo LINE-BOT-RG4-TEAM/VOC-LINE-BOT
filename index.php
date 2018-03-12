574b1aeac818077f20c328fa59b4eb55ce3728d8
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-03-12 22:44:24 +0700

    change index content

--- index.php
+++ index.php
@@ -18,16 +18,8 @@
     <body>
         <div data-role="page">
             <div data-role="header">
-                <h1>ระบบ VOC LINE BOT</h1>
+                <h1>Webhooks service - VOC BOT</h1>
             </div><!-- /header -->
-            
-            <div role="main" class="ui-content">
-                <p>Page content goes here.</p>
-            </div><!-- /content -->
-
-            <div data-role="footer">
-                <h4>Footer</h4>
-            </div><!-- /footer -->
         </div><!-- /page -->
     </body>
 </html>
