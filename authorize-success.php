915bf69820fcf83d51fbf5959c8b7977bf708966
tueseed <tue_seed@hotmail.com>
2018-03-13 18:24:20 +0700

    Add files via upload

---
+++ authorize-success.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<?php
+$name = $_POST['textfield3'];
+$lastname = $_POST['textfield2'];
+$position = $_POST['textfield'];
+$code = $_POST['password'];
+$url = parse_url(getenv("mysql://b9effd8882e09d:95317074@us-cdbr-iron-east-05.cleardb.net/heroku_c9af4d376fc017f?reconnect=true"));
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$conn = new mysqli($server, $username, $password, $db);
+mysqli_set_charset($conn,"utf8");
+$sql_insert ="INSERT INTO tbl_authorize (name, lastname, position, code)VALUES ('$name', '$lastname', '$position', '$code')";
+mysqli_query($conn,$sql_insert);
+mysqli_close($conn);
+?>
+</head>
+
+<body>
+</body>
+</html>
---
+++ authorize.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Authorize</title>
+
+<SCRIPT language="javascript">
+function randomStringp() {
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZ0123456789";
+var string_length = 8;
+var password = "";
+for (var i=0; i<string_length; i++) {
+var rnum = Math.floor(Math.random() * chars.length);
+password += chars.substring(rnum,rnum+1);
+}
+document.randgen.password.value = "#" + password;
+}
+</SCRIPT>
+</head>
+		<body>
+        		<div align="center">
+        			<h1>ลงทะเบียน VOC-BOT</h1>
+     			</div>
+        
+				<div align="center">
+                <form action="authorize-success.php" method="post">
+					<table width="600" border="1">
+ 						 <tr>
+    							<td width="300"><div align="right">ชื่อ</div></td>
+    							<td width="300"><input type="text" name="textfield3" id="textfield3" /></td>
+						 </tr>
+                         
+						 <tr>
+    							<td><div align="right">นามสกุล</div></td>
+    							<td><input type="text" name="textfield2" id="textfield2" /></td>
+   
+  						 </tr>
+                         
+ 						 <tr>
+    							<td><div align="right">ตำแหน่ง</div></td>
+    							<td><input type="text" name="textfield" id="textfield" /></td>
+    
+  						</tr>
+                        
+  						<tr>
+    							<td><div align="right">รหัสยืนยัน</div></td>
+   								<td><form name="randgen"><input name="password" id=”"password" type="text" onfocus="randomStringp()" value="" />
+         							<input name="" type="button" onClick="randomStringp()" value="GEN"></form>
+    							</td>
+                                
+    					<tr>
+    							<td ></td>
+        						<td><div align="center"><input type="submit" value="ตกลง" /></div></td>
+      					</tr>
+
+				</table>
+                </form>
+			</div>
+	</body>
+
+</html>
