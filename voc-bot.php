915453611d8d24c6db713c9ea9c494a3250f26ba
tueseed <tue_seed@hotmail.com>
2018-03-13 22:17:10 +0700

    Update voc-bot.php

--- voc-bot.php
+++ voc-bot.php
@@ -28,7 +28,25 @@ if (!is_null($events['events'])) {
 						$sql_regis = "UPDATE tbl_authorize SET line ='$lineid' WHERE code ='$text'";
 						mysqli_query($conn,$sql_regis);
 						mysqli_close($conn);
-			                        
+			                        $messages = [ 'type' => 'text',
+			                        	      'text' => "กรุณาตรวจสอบการใช้คำค้นหา"    
+                                                            ];
+			 			$url = 'https://api.line.me/v2/bot/message/reply';
+            					$data = [
+                					  'replyToken' => $replyToken,
+                					  'messages' => [$messages],
+            						];
+            					$post = json_encode($data);
+            					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+ 					        $ch = curl_init($url);
+            					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            					$result = curl_exec($ch);
+            					curl_close($ch);
+ 						echo $result . "\r\n";
 		        	        }
 //******************************************************************************************************//
 //----------------------------------------เอา UID ค้นหาในฐานข้อมูล-----------------------------------------//
