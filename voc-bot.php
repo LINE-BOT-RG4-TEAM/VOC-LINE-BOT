49552e12005ed09ee1cbb96d73a2f3f4866571f8
tueseed <tue_seed@hotmail.com>
2018-03-13 21:53:34 +0700

    Update voc-bot.php

--- voc-bot.php
+++ voc-bot.php
@@ -58,7 +58,7 @@ if (!is_null($events['events'])) {
 				 $main_office = substr($text,0,$addpos); 
 				 $datenum = substr($text,$addpos+1,$lengh1); 
 				 
-			     $sql = "SELECT * FROM tbl_complaint WHERE number_of_day<".$datenum." AND (office_name LIKE '%".$main_office."%' OR main_office LIKE '%".$main_office."%')";
+			     $sql = "SELECT * FROM tbl_complaint WHERE number_of_day<".$datenum." AND office_name LIKE '%".$main_office."%' OR main_office LIKE '%".$main_office."%'";
 			     $mode4 = "https://voc-bot.herokuapp.com/req_office.php?REQ=".$main_office."&REQ2=".$datenum;	 			
 				 	
 					 			 } 
