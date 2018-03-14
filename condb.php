eb33671510f79b0bfa83a492e3a8c1eaf613bdc5
tueseed <tue_seed@hotmail.com>
2018-03-14 21:58:35 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -5,9 +5,9 @@ $sql = "SELECT * FROM tbl_authorize WHERE code=".$codee;
 $query = mysqli_query($conn,$sql);
  while($result=mysqli_fetch_array($query))
 		 {
-	 $t = $result["line"];
+	 $t = $result['line'];
 		 	 }
-//mysqli_close($conn);
+mysqli_close($conn);
 echo "testtttttt     ".$t;
 
 ?>
