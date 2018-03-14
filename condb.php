a65232af34868187cf82d7d89da1d355b56281b2
tueseed <tue_seed@hotmail.com>
2018-03-14 22:33:07 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -3,11 +3,12 @@ require('./libs/database/connect-db.php');
 $codee = "#GCCW1G3Q";
 $sql = "SELECT * FROM tbl_authorize WHERE code LIKE '%".$codee."%'";
 $query = mysqli_query($conn,$sql);
+$mode1 = mysqli_num_rows($query);
  while($result = mysqli_fetch_array($query))
 		 {
 	 $t = $result['name'];
 		 	 }
 mysqli_close($conn);
-echo $t;
+echo $t."   ".$mode1;
 
 ?>
