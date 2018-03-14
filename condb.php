ff25b1edb169ddacb0ff6349b426deb881dd4a3e
tueseed <tue_seed@hotmail.com>
2018-03-14 21:52:23 +0700

    Update condb.php

--- condb.php
+++ condb.php
@@ -2,7 +2,7 @@
 require('./libs/database/connect-db.php');
 $codee = "#GCCW1G3Q";
 $sql = "SELECT * FROM tbl_authorize WHERE code=".$codee;
-//$query = mysqli_query($conn,$sql);
+$query = mysqli_query($conn,$sql);
  //while($result=mysqli_fetch_array($query))
 	//	 {$t = $result["name"]);
 		 	// }
