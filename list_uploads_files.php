8192221f71cd829485bc4b466edb503b1d32e9f0
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-03-31 20:22:10 +0700

    change to scandir

--- list_uploads_files.php
+++ list_uploads_files.php
@@ -1,5 +1,20 @@
 <?php
-    $xlsxList = glob("../*.*");
-    foreach($xlsxList as $file){
-        echo "<p>".realpath($file)."</p>";
-    }
+    // $xlsxList = glob("../*.*");
+    // foreach($xlsxList as $file){
+    //     echo "<p>".realpath($file)."</p>";
+    // }
+
+    $dir    = '/';
+    $files1 = scandir($dir);
+    echo '/<br/>';
+    print_r($files1);
+
+    $dir = './';
+    $files1 = scandir($dir);
+    echo './<br/>';
+    print_r($files1);
+
+    $dir = '../';
+    $files1 = scandir($dir);
+    echo '../<br/>';
+    print_r($files1);
