d5211399fa8f0255f12a289def553ceac43e43b4
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-04-20 22:21:12 +0700

    show userId

---
+++ fetch_profile_data.php
@@ -0,0 +1,54 @@
+<?php
+require('./libs/database/connect-db.php');
+$access_token = 'VXdNfb6gCuFsxYvfjvSIP0eO/Vt+6I6vy5Fy+kSkTyh5/lDJxgWe2LJWnZ2bD0HKK0ensLVlYgQgZgjY4tMb8ENo/gvJZXUfi2dGF9DGNgq38+4EIV8HF4Myi3msvqOroJXBppZYoiL38MWhAU3QUwdB04t89/1O/w1cDnyilFU=';
+ 
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+
+if (!is_null($events['events'])) {
+    // Loop through each event
+    foreach ($events['events'] as $event) {
+        // Reply only when message sent is in 'text' format
+        if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+            // Get text sent
+            $text = $event['message']['text'];
+            $lineid = $event['source']['userId'];
+
+            // $get_profile_ch = curl_init();
+            	
+            $messages = array(
+                'type'=> 'template',
+                // 'altText'=> "ค้นพบข้อร้องเรียน: ".$mode1." รายการ \nจากคำสั่ง: '$text'",
+                'template'=>array(
+                    'type'=>'buttons',
+                    'text'=> "$lineid"
+            ));
+	
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }
+    }
+}
+
+echo '<a href="test.php">test</a>';
+
