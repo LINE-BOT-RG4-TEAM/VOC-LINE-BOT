5fcd600974dd14b3ea72c34dffb5ee76b6f1e046
tueseed <tue_seed@hotmail.com>
2018-04-30 09:34:13 +0700

    Add files via upload

--- individual_notify.php
+++ individual_notify.php
@@ -9,14 +9,14 @@
 require('./libs/database/connect-db.php');
 require('./libs/utils/date_thai.php');
 require('./libs/utils/date_utils.php');
-$sql_select_officename = "SELECT * FROM tbl_complaint WHERE (complaint_status LIKE '%กำลังดำเนินการ%' OR complaint_status LIKE '%รอดำเนินการ%') AND (number_of_day >= 10) GROUP BY office_name";
+$sql_select_officename = "SELECT * FROM tbl_complaint WHERE (complaint_status LIKE '%กำลังดำเนินการ%' OR complaint_status LIKE '%รอดำเนินการ%') AND (number_of_day >= 0) GROUP BY office_name";
 $officename_list = mysqli_query($conn,$sql_select_officename);
 while($obj_office_name = mysqli_fetch_array($officename_list))
 {
-	echo $obj_office_name["office_name"]."".$obj_office_name["number_of_day"]."<br>";
+	echo $obj_office_name["office_name"]." ".$obj_office_name["number_of_day"]."<br>";
 	
 	}
-echo "sfdsfsefdsfe";
+
 
 
 ?>
