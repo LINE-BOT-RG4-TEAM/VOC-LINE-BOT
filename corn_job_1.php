72c58e34aca00748f0d786129c2674cf6f1abf9d
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 00:12:30 +0700

    fix

--- corn_job_1.php
+++ corn_job_1.php
@@ -1,4 +1,6 @@
 <?php
   require('./libs/database/connect-db.php');
-  $insert_comment = "INSERT INTO tbl_test_log(comment, insert_time) VALUES('corn_job_1', NOW())";
+  for($i=1;$i<=10;$i++){
+    $insert_comment = "INSERT INTO tbl_test_log(comment, insert_time) VALUES('corn_job_$i', NOW())";
+  }
   mysqli_query($conn, $insert_comment);
