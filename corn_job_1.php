5ce2e6d1a3bacd906b9ec675b2b6159e6b2d94fc
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 01:15:07 +0700

    edit cron job to push message

--- corn_job_1.php
+++ corn_job_1.php
@@ -1,6 +1,18 @@
 <?php
-  require('./libs/database/connect-db.php');
-  for($i=1;$i<=10;$i++){
-    $insert_comment = "INSERT INTO tbl_test_log(comment, insert_time) VALUES('corn_job_$i', NOW())";
-    mysqli_query($conn, $insert_comment);
-  }
+  $access_token = 'QPUPUnMzGhO//A8J2Qi1nmBXgEW89hciaaxNExeLVgxa8cjYtvnF9TZQF3TEjEOVA5HhS6dTRT2Tp4F0I3JhC0QWrQdmlBiL/6bhuazJI/juOxmvFx31NX7RWv9z19gbUZAdPIEuAURaHPy7TnDNkQdB04t89/1O/w1cDnyilFU=';
+  $messages = [ 'type' => 'text', 'text' => "Hello, this is push message."];
+  $url = 'https://api.line.me/v2/bot/message/push';
+  $data = [
+      'to' => "Ube5f4c7c18af1e4536d7b9bf6b7c15d1",
+      'messages' => [$messages],
+  ];
+  $post = json_encode($data);
+  $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  $result = curl_exec($ch);
+  curl_close($ch);
