05ad48db0cb17ce834ddcaaf528f82a3c14dcefb
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-24 23:35:03 +0700

    add double corn job to test on sametime

---
+++ corn_job_2.php
@@ -0,0 +1,4 @@
+<?php
+  require('./libs/database/connect-db.php');
+  $insert_comment = "INSERT INTO tbl_test_log(comment, insert_time) VALUES('corn_job_2', NOW())";
+  mysqli_query($conn, $insert_comment);
