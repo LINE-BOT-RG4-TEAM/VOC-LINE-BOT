4d3e85f95a87d7e5288b66ffcf870a23a71e2de5
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 15:45:05 +0700

    add favicon and manifest

--- south.php
+++ south.php
@@ -3,6 +3,9 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" charset="utf-8">
 		<title>รายการข้อร้องเรียน</title>
+		<link rel="manifest" href="/manifest.json">
+		<meta name="theme-color" content="#710E82">
+
 		<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
 		<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
 		<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
