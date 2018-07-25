b3da4e77e265b0fe19d8243d09d2592bc681d113
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 16:15:32 +0700

    #3

--- test.php
+++ test.php
@@ -7,7 +7,10 @@
 		<meta name="theme-color" content="#710E82">
 
 		<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+		<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+		<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
 		<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
+		<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
 	</head> 
 	<body> 
     <h1>ทดสอบ</h1>
