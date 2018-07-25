e46afcbcf5e84154137a3819e86927567576f7a1
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 18:43:33 +0700

    validate html5 format

--- south.php
+++ south.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html> 
-<html>
+<html lang="th">
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false">
+		<meta charset="utf-8" >
 		<title>รายการข้อร้องเรียน</title>
 		<link rel="manifest" href="/manifest.json">
 		<meta name="theme-color" content="#710E82">
 
 		<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
-		<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
-		<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
-		<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
-		<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
+		<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" />
+		<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" />
+		<script src="jquery-1.6.4.min.js" ></script>
+		<script src="jquery.mobile-1.0.min.js" ></script>
 		<script>
 			$(function(){
 				liff.init();
--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html> 
-<html>
+<html lang="th">
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" charset="utf-8">
-		<title>รายการข้อร้องเรียน</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" />
+		<meta charset="UTF-8">
+    <title>รายการข้อร้องเรียน</title>
 		<link rel="manifest" href="/manifest.json">
 		<meta name="theme-color" content="#710E82">
-
 		<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
 		<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" />
 		<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" />
