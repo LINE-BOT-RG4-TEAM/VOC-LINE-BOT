a5e5bdccecd40594c6ed8135e7335ba40ca10895
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-25 16:13:21 +0700

    test liff app

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" charset="utf-8">
+		<title>รายการข้อร้องเรียน</title>
+		<link rel="manifest" href="/manifest.json">
+		<meta name="theme-color" content="#710E82">
+
+		<script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+		<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
+	</head> 
+	<body> 
+    <h1>ทดสอบ</h1>
+	</body>
+</html>
