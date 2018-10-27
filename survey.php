926d664156f2488c4c093bc662169f8e50cc2316
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-10-27 11:31:48 +0700

    fix survey page layout

--- survey.php
+++ survey.php
@@ -1,97 +1,90 @@
 <html>
   <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>LIFF Starter</title>
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Survey Information</title>
+
+      <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   </head>
 
   <body>
-    <div class="buttongroup">
-      <div class="buttonrow">
-        <button id="getprofilebutton">Get Profile</button>
+    <div id="myModal" class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title" style='font-weight:bold;'>
+              Information
+            </h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="container">
+              <div class="row">
+                <div class="welcome-column col-md-8 offset-md-2">
+                  <div class="text-center">
+                    <h5 id="welcome-text" style="font-weight:bold;"></h5>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="first-column col-md-6 offset-md-3">
+                  <div class="text-center">
+                    <img id="avatar" style="height:100px;width:100px;border-radius: 50%;" />
+                    <div class="dropdown-divider"></div>
+                  </div>
+                </div>
+              </div>
+              <br/>
+              <div class="row">
+                <div class="col-12">
+                  <p style="text-indent: 20px" class="text-justify">
+                    เพื่อปรับปรุงโปรแกรมอัตโนมัติ 'VOC Alert Bot' ให้มีคุณภาพและประสิทธิผลมากยิ่งขึ้น ทางคณะผู้จัดทำ VOC Alert Bot กบว.(ภ4) 
+                    ได้จัดทำแบบสอบถาม เพื่อเป็นข้อมูลในการปรับปรุงโปรแกรมอัตโนมัติให้มีประสิทธิภาพ และตรงกับความต้องการมากยิ่งขึ้น
+                  </p>
+                  <p style="text-indent: 20px" class="text-justify">
+                    ทางคณะผู้จัดทำ VOC Alert Bot จึงใคร่ขอความอนุเคราะห์ผู้บริหารสายงานการไฟฟ้า ภาค 4 ทุกท่าน
+                    ตอบแบบสอบถาม จักขอบคุณยิ่ง
+                  </p>
+                  <p class="text-right font-weight-bold font-italic text-justify">
+                    คณะผู้จัดทำ VOC Alert Bot กบว.(ภ4)
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
       </div>
     </div>
-
-      <div id="profileinfo">
-        <h2>Profile</h2>
-        <div id="profilepicturediv">
-        </div>
-        <table border="1">
-          <tr>
-            <th>userId</th>
-            <td id="useridprofilefield"></td>
-          </tr>
-          <tr>
-            <th>displayName</th>
-            <td id="displaynamefield"></td>
-          </tr>
-          <tr>
-            <th>statusMessage</th>
-            <td id="statusmessagefield"></td>
-          </tr>
-        </table>
-      </div>
-
-      <div id="liffdata">
-          <h2>LIFF Data</h2>
-          <table border="1">
-              <tr>
-                  <th>language</th>
-                  <td id="languagefield"></td>
-              </tr>
-              <tr>
-                  <th>context.viewType</th>
-                  <td id="viewtypefield"></td>
-              </tr>
-              <tr>
-                  <th>context.userId</th>
-                  <td id="useridfield"></td>
-              </tr>
-              <tr>
-                  <th>context.utouId</th>
-                  <td id="utouidfield"></td>
-              </tr>
-              <tr>
-                  <th>context.roomId</th>
-                  <td id="roomidfield"></td>
-              </tr>
-              <tr>
-                  <th>context.groupId</th>
-                  <td id="groupidfield"></td>
-              </tr>
-          </table>
-      </div>
-      
-      <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
-      <script>
-      window.onload = function (e) {
-        liff.init(function (data) {
-            initializeApp(data);
-        });
-      };
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+    <script>
       function showLINEProfile(){
         liff.getProfile().then(function (profile) {
-          document.getElementById('useridprofilefield').textContent = profile.userId;
-          document.getElementById('displaynamefield').textContent = profile.displayName;
+          var welcome_text = document.getElementById("welcome-text");
+          welcome_text.textContent = "ยินดีต้อนรับ คุณ " + profile.displayName;
 
-          var profilePictureDiv = document.getElementById('profilepicturediv');
-          if (profilePictureDiv.firstElementChild) {
-              profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-          }
-          var img = document.createElement('img');
-          img.src = profile.pictureUrl;
-          img.alt = "Profile Picture";
-          profilePictureDiv.appendChild(img);
-
-          document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+          var avatar = document.getElementById("avatar");
+          avatar.src = avatar.src = profile.pictureUrl;
         }).catch(function (error) {
             window.alert("Error getting profile: " + error);
         });
       }
-      showLINEProfile()
-      </script>
+    window.onload = function (e) {
+      liff.init(function (data) {
+        showLINEProfile()
+        $('#myModal').modal('show')
+        $("#myModal").on('hidden.bs.modal', function(){
+          window.location = "https://goo.gl/forms/1TRgTk6TWUpx5O072"
+        })
+      });
+    };
+    </script>
   </body>
 </html>
