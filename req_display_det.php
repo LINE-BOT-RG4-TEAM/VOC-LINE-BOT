e3e451e9bf036c31875e3b0fc0fd9172fc3eb058
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-02-14 00:02:43 +0700

    add seconds format

--- req_display_det.php
+++ req_display_det.php
@@ -18,13 +18,13 @@
 				$strYear = date("Y",strtotime($strDate))+543;
 				$strMonth= date("n",strtotime($strDate));
 				$strDay= date("j",strtotime($strDate));
-				//$strHour= date("H",strtotime($strDate));
-				//$strMinute= date("i",strtotime($strDate));
-				//$strSeconds= date("s",strtotime($strDate));
+				$strHour= date("H",strtotime($strDate));
+				$strMinute= date("i",strtotime($strDate));
+				$strSeconds= date("s",strtotime($strDate));
 				// $strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
 				$strMonthCut = Array("","มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม");
 				$strMonthThai=$strMonthCut[$strMonth];
-				return "$strDay $strMonthThai $strYear";
+				return "$strDay $strMonthThai $strYear, $strHour:$strMinute น.";
 			}
 
 			function DateDiff($strDate1){
