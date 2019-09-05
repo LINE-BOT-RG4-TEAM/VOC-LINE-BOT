c796fdbc3b1c66a2d6f538bcd493122860ea2e6c
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-09-05 11:32:33 +0700

    change redirect uri

--- notify_authorize_callback.php
+++ notify_authorize_callback.php
@@ -1,18 +1,12 @@
 <?php
-    function siteURL(){
-        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-        $domainName = $_SERVER['HTTP_HOST'].'/';
-        return $protocol.$domainName;
-    }
-
     $CLIENT_ID = getenv("LINE_NOTIFY_CLIENT_ID");
-
     $payload = base64_decode($_GET['payload']);
+    $redirect_uri = "https://voc-bot.herokuapp.com/notify-callback.php";
 
     $params = array(
         'response_type' => 'code',
         'client_id' => $CLIENT_ID,
-        'redirect_uri' => siteURL()."/notify-callback.php",
+        'redirect_uri' => $redirect_uri."/notify-callback.php",
         'scope' => 'notify',
         'state' => $payload
     );
