a690c9dff4a1a5a7220ba8550bc63139ebab25e5
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-09-06 01:11:05 +0700

    add CURLOPT_POST opt

--- daily_notify.php
+++ daily_notify.php
@@ -31,9 +31,9 @@
         $ch = curl_init($notify_uri);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, count($message));
         curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
         curl_close($ch);
     }
