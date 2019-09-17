875baf05f5f8c6a3dfb1bbacd2ff155c5fcc5c8e
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2019-09-17 23:06:12 +0700

    fix string to alert complaint from notify

--- daily_notify.php
+++ daily_notify.php
@@ -13,6 +13,27 @@
         return;
     }
 
+    // get complaint results
+    $fetch_existing_complaint = "SELECT main_office, COUNT(main_office) AS count_complaint ".
+                                "FROM tbl_complaint ".
+                                "WHERE number_of_day>='10' AND complaint_status <> 'ปิด' ".
+                                "GROUP BY main_office ".
+                                "HAVING COUNT(main_office) > 0 ".
+                                "ORDER BY main_office ASC";
+    $complaint_list = mysqli_query($conn, $fetch_existing_complaint);
+    if(mysqli_num_rows($complaint_list) > 0){
+        $prepare_message = "\n\n·รายงานข้อร้องเรียนสถานะรอและกำลังดำเนินการมากกว่าเท่ากับ 10 วัน \n\nประจำวันที่ ".DateThai(date("Y-m-d"));
+        $prepare_message .= getStringComplaintList($complaint_list);
+        $prepare_message .= "\n\nhttps://voc-bot.herokuapp.com/south.php?NUMBER=@10";
+        $message = [
+            "message" => $prepare_message
+        ];
+    } else {
+        $message = [
+            "message" => "\n\nไม่มีข้อร้องเรียนสถานะกำลังดำเนินการหรือรอดำเนินการที่มากกว่าเท่ากับ 10 วัน ในวันที่ ".DateThai(date("Y-m-d"))
+        ];
+    }
+
     $fetch_notify_list = "SELECT `target`, access_token FROM tbl_notify WHERE `status` = 'A'";
     $notify_list = mysqli_query($conn, $fetch_notify_list) or die($fetch_notify_list);
 
@@ -23,10 +44,6 @@
             "Authorization: Bearer ".$access_token
         ];
 
-        $message = [
-            "message" => "\n\nรายงานข้อร้องเรียนสถานะรอและกำลังดำเนินการมากกว่าเท่ากับ 10 วัน \n\nประจำวันที่ ".DateThai(date("Y-m-d"))." \n\nhttps://voc-bot.herokuapp.com/south.php?NUMBER=@10"
-        ];
-
         $ch = curl_init($notify_uri);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -35,4 +52,12 @@
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         $result = curl_exec($ch);
         curl_close($ch);
+    }
+
+    function getStringComplaintList($complaint_list){
+        $complaint_string_list = "";
+        while($complaint = $complaint_list->fetch_assoc()){
+            $complaint_string_list .= "\n  · {$complaint_list["main_office"]} จำนวน `{$complaint_list[count_complaint]} เรื่อง`";
+        }
+        return $complaint_string_list;
     }
